5016115408175606d1aa14bcc52a1650aa6a5ccc
chavezfk <chavezfk@gmail.com>
2020-09-19 14:12:54 -0600

    add a goal placeholder

---
+++ Assets/GoalController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("trigger entered!");
+        if (collision.gameObject.tag == "Player")
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
