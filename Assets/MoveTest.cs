7c35d6072290610d23d0c20d6452940a20af7818
chavezfk <chavezfk@gmail.com>
2020-09-19 11:44:35 -0600

    MVP of movement.

---
+++ Assets/MoveTest.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+public class MoveTest : MonoBehaviour
+{
+    public GameObject pointer;
+    private bool spawnedPointer = false;
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 w = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+
+            if (!spawnedPointer)
+            {
+                w.z = 0;
+                GameObject.Instantiate(pointer, w, Quaternion.identity);
+                spawnedPointer = true;
+            }
+            GetComponent<NavMeshAgent2D>().destination = w;
+        }
+        else
+        {
+            spawnedPointer = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("trigger entered!");
+        if (collision.gameObject.tag == "Player")
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/pointerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pointerController : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("trigger entered!");
+        if (collision.gameObject.tag == "Player")
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+
+}
