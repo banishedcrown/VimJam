f4ac84edfabc10d17139c30bd10a2e42efc9360f
TAILSAWAIT\Anna <careya42>
2020-09-20 01:26:53 -0600

    Added enemy and moved scripts

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    enum EnemyState { IDLE, WANDER, PERSUE };
+    EnemyState state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Patrol, or wander
+        switch(state)
+        {
+            case EnemyState.IDLE:
+                break;
+            case EnemyState.WANDER:
+                break;
+            case EnemyState.PERSUE:
+                break;
+            default: break;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //Switch states here
+    }
+
+
+
+    
+}
