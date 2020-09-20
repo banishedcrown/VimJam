c49589c8b50d2467321cecdb78f195d5a055a234
TAILSAWAIT\Anna <careya42>
2020-09-20 02:00:54 -0600

    Started adding states to EnemyAI

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -2,22 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class EnemyAI : MonoBehaviour
 {
-    enum EnemyState { IDLE, WANDER, PERSUE };
-    EnemyState state;
+    private enum EnemyState { IDLE, WANDER, PERSUE };
+    private EnemyState state;
+    private QuickTimer timer;
+    private float idleTime = 3; //3s
+    private float wanderTime = 8; //8s
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = new QuickTimer();
     }
 
     // Update is called once per frame
     void Update()
     {
         //Patrol, or wander
-        switch(state)
+        switch (state)
         {
             case EnemyState.IDLE:
                 break;
@@ -31,10 +34,30 @@ public class Enemy : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if(state == EnemyState.PERSUE && canSeePlayer())
+        {
+            return;
+        }
+        else
+        {
+            state = EnemyState.IDLE;
+            timer.Reset();
+        }
+        if(state == EnemyState.IDLE && timer.elapsed > idleTime)
+        {
+            state = EnemyState.WANDER;
+            timer.Reset();
+        }
+        else if(state == EnemyState.WANDER && timer.elapsed > wanderTime)
+        {
+            state = EnemyState.IDLE;
+            timer.Reset();
+        }
         //Switch states here
     }
 
+    private bool canSeePlayer()
+    {
 
-
-    
-}
+        return false;
+    }
