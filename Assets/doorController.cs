00d12ec57d82c49f45d1ffecba9fb3764bffd968
chavezfk <chavezfk@gmail.com>
2020-09-23 21:37:21 -0600

    door controls, and idle animations. up and running!

---
+++ Assets/doorController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Door trigger entered!");
+    }
+}
