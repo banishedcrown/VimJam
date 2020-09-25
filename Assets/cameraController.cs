f0c701a3e0897f06452ac453b6f3fc232ff934d9
chavezfk <chavezfk@gmail.com>
2020-09-25 01:36:28 -0600

    audio work, and fully tested gameplay loop.

--- Assets/cameraController.cs
+++ Assets/cameraController.cs
@@ -19,11 +19,24 @@ public class cameraController : MonoBehaviour
     }
 
     // Update is called once per frame
+    public bool reverse = true;
     void Update()
     {
-        if(player.transform.position.y > StartFollowPosition && player.transform.position.y < EndFollowPosition)
+        if (!reverse)
         {
-            transform.position = startPosition + new Vector3(0, player.transform.position.y - StartFollowPosition, 0);
+            if (player.transform.position.y > StartFollowPosition && player.transform.position.y < EndFollowPosition)
+            {
+                transform.position = startPosition + new Vector3(0, player.transform.position.y - StartFollowPosition, 0);
+            }
+        }
+
+        else
+        {
+            if (player.transform.position.y > StartFollowPosition && player.transform.position.y < EndFollowPosition)
+            {
+                Vector3 position = new Vector3(transform.position.x, player.transform.position.y, -100);
+                transform.position = position;
+            }
         }
     }
 }
