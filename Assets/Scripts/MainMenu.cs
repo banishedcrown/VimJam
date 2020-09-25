2e78ae56915780b2192a6bac16b94eba3e1ae111
ibrowmew <ibrowmew@gmail.com>
2020-09-24 21:35:36 -0600

    Finalized Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+        Debug.Log("Playing Game");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void MainMenuBack()
+    {
+        Debug.Log("GoingBack");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+
+    public void WebsiteURL()
+    {
+        Debug.Log("URL PROC");
+        Application.OpenURL("https://www.banishedcrown.com");
+    }
+    public void QuitGame ()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+}
