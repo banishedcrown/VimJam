b11ba91a48efabad3499f8dbd0514ae31921618a
chavezfk <chavezfk@gmail.com>
2020-09-24 22:35:18 -0600

    coin stuff and music.

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
 
-    public AudioClip clip; 
+    public AudioClip[] clip; 
 
     public void playBurst()
     {
-        gameObject.GetComponent<AudioSource>().PlayOneShot(clip);
+        gameObject.GetComponent<AudioSource>().PlayOneShot(clip[0]);
+    }
+
+    public void playIndex(int index)
+    {
+        gameObject.GetComponent<AudioSource>().PlayOneShot(clip[index]);
     }
 
 }
