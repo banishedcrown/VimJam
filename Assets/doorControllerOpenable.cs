024a09f4a11c33aea9ffb05e65b2d1d733f9a97d
chavezfk <chavezfk@gmail.com>
2020-11-27 17:08:05 -0700

    stuff with tiles

---
+++ Assets/doorControllerOpenable.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class doorControllerOpenable: MonoBehaviour
+{
+    public bool open = false;
+    public string newScene;
+
+    GameProgressTracker manager;
+
+    private void Start()
+    {
+        GameObject m = GameObject.FindGameObjectWithTag("Manager");
+        if (m != null) {
+            manager = m.GetComponent<GameProgressTracker>();
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!needsCompletion)
+        {
+            if (manager != null)
+            {
+                if (!manager.canReturn)
+                    SceneManager.LoadScene(newScene);
+            }
+            else
+            {
+                SceneManager.LoadScene(newScene);
+            }
+        }
+
+        else
+        {
+            //do nothing right now.
+            if (manager != null)
+            {
+                if (manager.canReturn)
+                {
+                    SceneManager.LoadScene(newScene);
+                }
+            }
+        }
+    }
+}
